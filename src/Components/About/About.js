f64126a4b6c8ea6b59f1f410c53efdf7240d4ee3
Vijay1110 <33751735+Vijay1110@users.noreply.github.com>
2020-08-09 20:53:25 +0530

    Add files via upload

---
+++ src/Components/About/About.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import "./About.css";
+
+const About = () => {
+  return (
+    <div id="about-me" className="about  mb-5  d-flex flex-column">
+      <div className="display-4 align-self-start mb-4 about-text">About Me</div>
+
+      <div className="h5 title text-justify mb-4">
+        PERFECTION IS ACHIEVED NOT WHEN THESE IS NOTHING MORE TO DO ADD,BUT WHEN
+        THERE IS NOTHING LEFT TO TAKE AWAY.
+      </div>
+      <div className="body text-justify">
+        Pursuing my masters in computer application.
+        <br />
+        <br />
+        Since beginning my journey as a freelance web developer nearly 3 years
+        ago, I've done remote work for friends and for know peoples. I'm quietly
+        confident, naturally curious, and perpetually working on improving my
+        tech skill to next level.
+        <br />
+        <br /> During my college time, I took online courses like Advanced CSS
+        and Sass: Flexbox, Grid, Animations and More! The Complete Javascript
+        Course, Javascript30, Understanding the Weird Parts, ES6, Modern React
+        w/ Redux, React: The Complete Guide, Gatsby Bootcamp, Angular by mosh,
+        Mean Stack by maximilian schwarzmüller ,read article series "Clean
+        Code", and also watched countless youtube videos about Javascript.
+        <br />
+        <br />
+        Because we know the best way to learn is to practice, I also did a bunch
+        of personal projects with the knowledge I gathered from the courses, you
+        can check them on my portfolio.
+        <br />
+        <br />I always want to learn more, do more, and be more. I’m also a firm
+        believer that we should never settle. I’m hard working, super curious,
+        passionate, committed, and also a fast learner!
+      </div>
+    </div>
+  );
+};
+
+export default About;
